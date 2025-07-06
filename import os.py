8867f7712b1b4173a1f68abcbe280845c027e77e
imabanana23 <alihina935@gmail.com>
2025-07-06 11:47:07 +0500

    Add files via upload

---
+++ import os.py
@@ -0,0 +1,5 @@
+import os
+print("checking to see if my file is real or not")
+my_file=open("C:/Users/HP/Desktop/hobbies123.txt","w")
+my_file.write("i dont have hobbies all i just sleep all day")
+my_file.close()
